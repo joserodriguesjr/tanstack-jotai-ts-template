744689897ba1f24f0b0ab72b95efcd46d99e5b20
runiorr <joseluisrjunior@gmail.com>
2025-03-28 19:06:33 -0300

    improving header skeleton

--- app/widgets/pokemon/pokemon-header/pokemon-header.skeleton.tsx
+++ app/widgets/pokemon/pokemon-header/pokemon-header.skeleton.tsx
@@ -4,30 +4,7 @@ export const PokemonHeaderSkeleton = () => {
   return (
     <header className="sticky top-0 z-50 bg-gray-800 shadow-md">
       <div className="mx-auto flex max-w-6xl items-center justify-between p-4">
-        {/* Logo */}
-        <Skeleton className="h-8 w-32" />
-
-        {/* Botão de reload */}
-        <Skeleton className="h-8 w-8 rounded-full" />
-
-        {/* Troca de idioma */}
-        <Skeleton className="h-6 w-12 rounded-full" />
-
-        {/* Troca de tema */}
-        <Skeleton className="h-8 w-8 rounded-full" />
-
-        {/* Barra de pesquisa */}
-        <Skeleton className="h-10 w-48 rounded-md" />
-
-        {/* Menu desktop */}
-        <nav className="hidden gap-6 md:flex">
-          <Skeleton className="h-6 w-24" />
-          <Skeleton className="h-6 w-24" />
-          <Skeleton className="h-6 w-24" />
-        </nav>
-
-        {/* Menu mobile (hambúrguer) */}
-        <Skeleton className="h-8 w-8 rounded-full md:hidden" />
+        <Skeleton className="h-8 w-full" />
       </div>
     </header>
   );
